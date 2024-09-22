9f967f2ae24cb7af27c1c4acdc6f89f57c0ac73c
AbhijeetSingh10 <71282822+AbhijeetSingh10@users.noreply.github.com>
2024-09-22 12:25:10 +0530

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
+// creating server 
+// A server is a system (either hardware or software) that provides resources, data, services,
+//  or programs to other computers, known as clients, over a network. In the context of web
+//  development, a server typically handles HTTP requests from clients (like web browsers)
+//  and serves back the requested content (like web pages or APIs).
+
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Database Connection
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');    
+})
+.catch((err) => {
+    console.log(err);
+});
+
 
 const app = express();
 
 app.listen(3000, ()=> {
-    console.log("Server listening on port 3000!");
+    console.log("Server listening on port 3000");
     
 });
