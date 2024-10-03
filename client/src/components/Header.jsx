351e144809b49a50ffbc34c186881ea708c11403
AbhijeetSingh10 <71282822+AbhijeetSingh10@users.noreply.github.com>
2024-10-03 10:25:20 +0530

    update header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 // In React Router, the Link component is used to create navigation links between different routes in your application without
 // causing a full page reload. This allows for a smoother and faster user experience.
 
 function Header() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <div className="bg-slate-200">
          <div className="flex justify-between items-center max-w-6xl mx-auto p-3">       {/*tailwind css class*/}
@@ -18,8 +19,12 @@ function Header() {
               <Link to='/about'>
               <li>About</li>
               </Link>
-              <Link to='/sign-in'>
+              <Link to='/profile'>
+              {currentUser ? (
+                <img src= {currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+              ) : (
               <li>Sign In</li>
+              )}
               </Link>
             </ul>
         </div> 
